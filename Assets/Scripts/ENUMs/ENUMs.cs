4286873fa2d4e16e4bc5c4236a4e23fd85c92190
mQ <m.jaxygulov@gmail.com>
2026-05-11 23:28:56 +0500

    Sprint 9-10

---
+++ Assets/Scripts/ENUMs/ENUMs.cs
@@ -0,0 +1,20 @@
+public enum E_QuestionType
+{
+    WhatHappenedBeforeYouGotHere, //1
+    WhatKindOfLifeDidYouLive, //2
+    WhatWasYourJob, //3
+    WhatHappenedToYourClothes, //4
+    WhyDoYouHaveAKnife, //5
+}
+
+public enum E_CharacterSins
+{
+    None,
+    Pride,
+    Envy,
+    Wrath,
+    Sloth,
+    Greed,
+    Gluttony,
+    Lust
+}
---
+++ Assets/Scripts/ScriptableObjects/AnswerOptions.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Answer Options", menuName = "ScriptableObjects/Answer Options")]
+public class AnswerOptions : ScriptableObject
+{
+    [TextArea(4,4)] [Header("What Happened Before You Got Here")]
+    public string Answer1Text;
+    [TextArea(4,4)] [Header("What Kind of Life Did You Live")]
+    public string Answer2Text;
+    [TextArea(4,4)] [Header("What Was Your Job")]
+    public string Answer3Text;
+    [TextArea(4,4)] [Header("What Happened To Your Clothes")]
+    public string Answer4Text;
+    [TextArea(4,4)] [Header("Why Do You Have A Knife")]
+    public string Answer5Text;
+
+}
