42c827b4f83a68468044fb95ae1c757a1e258e3d
aksu <ak.amankeldi@student.nyfa.edu>
2026-05-19 13:24:07 +0500

    Click Script

---
+++ Assets/Scripts/Clicks.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Clicks : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Touchscreen.current != null)
+            {
+                if(Touchscreen.current.primaryTouch.press.wasPressedThisFrame)
+                {
+                    Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
+                    Ray ray = Camera.main.ScreenPointToRay(touchPosition);
+                    if(Physics.Raycast(ray, out RaycastHit hit))
+        {
+            Debug.Log($"Tapped on object: {hit.transform.name}");
+        }
+        
+        ///DoSomething(hit.point);
+                }
+            }
+    }
+}
